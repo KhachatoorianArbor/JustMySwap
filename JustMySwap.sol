ceac4166070dd0a201e2a8ba2c60d83adc12e06a
KhachatoorianArbor <101538723+KhachatoorianArbor@users.noreply.github.com>
2023-11-21 07:57:47 +0100

    Update JustMySwap.sol

--- JustMySwap.sol
+++ JustMySwap.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TokenSwap is Ownable {
+    using SafeERC20 for IERC20;
+
+    IERC20 public tokenA;
+    IERC20 public tokenB;
+
+    event TokensSwapped(address indexed sender, uint256 amountA, uint256 amountB);
+
+
+}
 
